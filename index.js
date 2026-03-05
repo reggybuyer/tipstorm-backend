4c74d57306f888170213f7aaf9e3e540bad387f9
reggybuyer <youremail@example.com>
2026-03-05 14:26:17 +0300

    fix port binding

--- index.js
+++ index.js
@@ -292,4 +292,4 @@ app.post('/slip-result', verifyAdmin, async (req, res) => {
 
 // Server
 const PORT = process.env.PORT || 5000
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`)) 
+app.listen(PORT,"0.0.0.0", () => console.log(`Server running on port ${PORT}`)) 
